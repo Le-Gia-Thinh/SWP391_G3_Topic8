32dda944f212942e3fab69888b77e8a0dacb10ea
nlvduyy180404-coder <nlvduyy180404@gmail.com>
2026-06-22 00:01:25 +0700

    Update GEMINI_API_KEY and cleanup test files

--- BE/test_chat.js
+++
@@ -1,9 +0,0 @@
-import { processChat } from './src/services/aiChatService.js';
-import dotenv from 'dotenv';
-dotenv.config();
-
-async function test() {
-  const result = await processChat([{ role: 'user', content: 'hello' }]);
-  console.log("Result:", result);
-}
-test();
